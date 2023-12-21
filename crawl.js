bbe4e6898c0d15d14ce09c70985849878b93459b
Suhel Khan <suhelk3003btechiot2026@kccitm.edu.in>
2023-12-21 18:08:42 +0530

    added code for invalid,relative URLs and test for it

--- crawl.js
+++ crawl.js
@@ -1,3 +1,33 @@
+const { JSDOM } = require("jsdom");
+
+function getURLsFromHTML(htmlBody, baseURl) {
+  const urls = [];
+
+  const dom = new JSDOM(htmlBody);
+  const linkElements = dom.window.document.querySelectorAll("a");
+
+  for (const linkElement of linkElements) {
+    if (linkElement.href.slice(0, 1) === "/") {
+      //It's relative boy
+      try {
+        const urlObj = new URL(`${baseURl}${linkElement.href}`);
+        urls.push(urlObj.href);
+      } catch (err) {
+        console.log(`error with relative url:${err.message}`);
+      }
+    } else {
+      //It's absiolute boy
+      try {
+        const urlObj = new URL(linkElement.href);
+        urls.push(urlObj.href);
+      } catch (err) {
+        console.log(`error with absolute url:${err.message}`);
+      }
+    }
+  }
+  return urls;
+}
+
 function normalizeURL(urlString) {
   const urlObj = new URL(urlString);
   const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
@@ -9,4 +39,5 @@ function normalizeURL(urlString) {
 
 module.exports = {
   normalizeURL,
+  getURLsFromHTML,
 };
--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const { normalizeURL } = require("./crawl.js");
+const { normalizeURL, getURLsFromHTML } = require("./crawl.js");
 const { test, expect } = require("@jest/globals");
 
 test("normalizeURL strip protocol", () => {
@@ -26,5 +26,50 @@ test("normalizeURL strip http", () => {
   const input = "http://koursera.netlify.app/home/";
   const output = normalizeURL(input);
   const expected = "koursera.netlify.app/home";
+  expect(output).toEqual(expected);
+});
+
+test("getURLsFromHTML absolute", () => {
+  const inputHTMLBody = `
+  <html>
+        <body>
+              <a href="https://koursera.netlify.app/path"> Click Me </a>
+        </body>
+  </html>
+  `;
+
+  const inputBaseURL = "https://koursera.netlify.app/path";
+  const output = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+  const expected = ["https://koursera.netlify.app/path"];
+  expect(output).toEqual(expected);
+});
+
+test("getURLsFromHTML relative", () => {
+  const inputHTMLBody = `
+  <html>
+        <body>
+              <a href="/path"> Click Me </a>
+        </body>
+  </html>
+  `;
+
+  const inputBaseURL = "https://koursera.netlify.app";
+  const output = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+  const expected = ["https://koursera.netlify.app/path"];
+  expect(output).toEqual(expected);
+});
+
+test("getURLsFromHTML invalid", () => {
+  const inputHTMLBody = `
+  <html>
+        <body>
+              <a href="invalid"> Click Me </a>
+        </body>
+  </html>
+  `;
+
+  const inputBaseURL = "https://koursera.netlify.app";
+  const output = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+  const expected = [];
   expect(output).toEqual(expected);
 });
