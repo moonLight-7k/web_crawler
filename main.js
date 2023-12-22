45aa27ffceacaaeb8c4b56cd217105e1050766d4
Suhel Khan <suhelk3003btechiot2026@kccitm.edu.in>
2023-12-22 11:16:23 +0530

    optimize main.js for error handling

--- main.js
+++ main.js
@@ -1,18 +1,28 @@
 const { crawlPage } = require("./crawl.js");
 
 async function main() {
-  if (process.argv.length !== 3) {
-    console.error("ⅹ Usage: node main.js <website>");
+  if (process.argv.length < 3) {
+    console.error("× Usage: node main.js <website>");
     process.exit(1);
   }
 
   const baseUrl = process.argv[2];
 
+  //  ========|Experimental Feature|=======
+  // const targetTag = process.argv[3];
+  // if (targetTag == "Text" || targetTag == "text") {
+  //   console.log("✓ ======Getting Text======");
+  // }
+
   console.log(`✓ Starting crawler for ${baseUrl}....`);
 
-  const pages = await crawlPage(baseUrl, baseUrl, {});
-  for (const page of Object.entries(pages)) {
-    console.log(page);
+  try {
+    const pages = await crawlPage(baseUrl, baseUrl, {});
+    for (const [page, count] of Object.entries(pages)) {
+      console.log(`${page}: ${count} visits`);
+    }
+  } catch (error) {
+    console.error(`𐄂 Error in main function: ${error.message}`);
   }
 }
 
